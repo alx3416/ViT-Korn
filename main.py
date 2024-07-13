6e389db84465cd04ff6b283d6b8141518ecc3d84
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-07-13 00:28:55 -0600

    adding plot and text generation for trainning and validation

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class_names = pre.get_classes_names(image_datasets)
 device = pre.set_device()
 
 inputs, classes = pre.get_training_batch_sample(dataloaders)
-vis.imshow(inputs, title=[class_names[x] for x in classes])
+# vis.imshow(inputs, title=[class_names[x] for x in classes])
 
 model_ft = mdll.set_model()
 model_ft = mdll.set_transfer(model_ft, class_names)
@@ -36,8 +36,12 @@ optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-model_ft = mdll.set_train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                                dataloaders, device, dataset_sizes,
-                                num_epochs=ini.EPOCHS)
+model_ft, train_loss, train_acc, val_loss, val_acc = mdll.set_train_model(model_ft, criterion, optimizer_ft,
+                                                                          exp_lr_scheduler,
+                                                                          dataloaders, device, dataset_sizes,
+                                                                          num_epochs=ini.EPOCHS)
 
-torch.save(model_ft.state_dict(), 'out/'+ini.MODEL+'.pt')
+torch.save(model_ft.state_dict(), 'out/' + ini.MODEL + '.pt')
+mdll.save_historic_data(train_loss, train_acc, val_loss, val_acc)
+vis.save_plot_losses(train_loss, val_loss)
+vis.save_plot_accuracies(train_acc, val_acc)
