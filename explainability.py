5e2a33dd788baed6e4d212218474cd84346c0f89
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2025-03-19 13:51:38 -0600

    to check later

--- explainability.py
+++ explainability.py
@@ -7,13 +7,18 @@ import numpy as np
 import torch
 from torchsummary import summary
 
-model_name = "mobilenet_v3_large"
+model_name = "googlenet"
 # model = resnet50(pretrained=True)
 model_path = "out/" + model_name + "/" + model_name + ".pt"
 model = torch.load(model_path)
 model.eval()
 print(model)
-target_layers = [model.features[-1]]
+# resnet50
+# target_layers = [model.module.layer4[-1]]
+# googlenet
+target_layers = [model.module.inception5b]
+# MobileNet
+# target_layers = [model.module.features[-1]]
 
 rgb_img = cv2.imread("data/corn/test/01_Cercospora_leaf_spot/2174d811-b088-4b31-8088-1738630b94d4___RS_GLSp 4467.JPG", 1)[:, :, ::-1]
 rgb_img = cv2.resize(rgb_img, (224, 224))
