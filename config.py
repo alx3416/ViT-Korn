967478df74316b6c4f2dd36960283d41b728d158
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2025-03-19 13:59:23 -0600

    adding dataset path

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # dataset options
-DATA_DIR = 'data/corn'
+DATA_DIR = 'data/NTU'
 OUTPUT_DIR = 'out'
-INPUT_SIZE = 224
+INPUT_SIZE = 512
 SPLITS = ['train', 'val']
 
 # Training options
 # Available models:
 # googlenet, mobilenet_v3_large, resnet50, swin_v2_b, vit_b_16, vit_b_32
 MODEL = ["googlenet", "mobilenet_v3_large", "resnet50", "swin_v2_b", "vit_b_16", "vit_b_32"]
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 NUM_WORKERS = 4
 EPOCHS = 25
